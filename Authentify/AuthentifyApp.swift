21798e3f4d6297956bb48edb286bf707a737c78b
ChiranMC <wilegoda1234@gmail.com>
2025-04-10 20:19:31 +0530

    Initial Commit

---
+++ Authentify/AuthentifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthentifyApp.swift
+//  Authentify
+//
+//  Created by Knight.Wolf on 2025-04-10.
+//
+
+import SwiftUI
+
+@main
+struct AuthentifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthentifyTests/AuthentifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  AuthentifyTests.swift
+//  AuthentifyTests
+//
+//  Created by Knight.Wolf on 2025-04-10.
+//
+
+import Testing
+@testable import Authentify
+
+struct AuthentifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
